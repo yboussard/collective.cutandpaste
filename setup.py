4b72683305f437840a9ce6a97f849ad32606db14
Boussard <y.boussard@free.fr>
2011-11-24 15:33:47 +0100

    add some info for release

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Alterway Solutions 
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, write to the
+# Free Software Foundation, 
+# 51 Franklin Street, Suite 500, Boston, MA 02110-1335,USA
+
 from setuptools import setup, find_packages
 import os
 
@@ -11,11 +29,15 @@ setup(name='collective.cutandpaste',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
+          "Programming Language :: Python",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)"
         ],
-      keywords='',
-      author='',
-      author_email='',
+      keywords='transmogrifier plone',
+      author='yboussard',
+      author_email='y.boussard@free.fr',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
